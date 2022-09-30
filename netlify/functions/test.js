f3a16e8982cff6e2272222dc595700747afdf05d
icebox246 <iceboxpoczta@gmail.com>
2022-09-30 23:33:45 +0200

    Add importing to test function

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,15 @@
+import {XMLParser} from 'fast-xml-parser';
+
+
+const xml = "<note> <to>Tove</to> <from>Jani</from> <heading>Reminder</heading> <body>Don't forget me this weekend!</body> </note>";
+
+
 export async function handler() {
+	const parser = new XMLParser();
+	const json = parser.parse(xml);
+
 	return {
 		statusCode: 200,
-		body: "Hello"
+		body: JSON.stringify(json)
 	}
 }
