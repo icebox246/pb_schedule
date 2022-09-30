2b28d509f6f7b1d92b5e90523a94c3ea469fc88f
icebox246 <iceboxpoczta@gmail.com>
2022-09-30 23:40:06 +0200

    Add fetching to test function

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,15 +1,13 @@
 import {XMLParser} from 'fast-xml-parser';
-
-
-const xml = "<note> <to>Tove</to> <from>Jani</from> <heading>Reminder</heading> <body>Don't forget me this weekend!</body> </note>";
+import fetch from 'node-fetch';
 
 
 export async function handler() {
-	const parser = new XMLParser();
-	const json = parser.parse(xml);
+    const xml = await (await fetch('https://www.w3schools.com/xml/note.xml'))
+    const parser = new XMLParser();
+    const json = parser.parse(xml);
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify(json)
-	}
+    return {
+        statusCode: 200, body: JSON.stringify(json)
+    }
 }
