de2492ba2448e6f168d06c3f1663a2aa8b8be9e6
icebox246 <iceboxpoczta@gmail.com>
2022-09-30 22:43:26 +0200

    Fix issue with top-level await

--- src/main.js
+++ src/main.js
@@ -6,27 +6,29 @@ import App from './App.vue'
 import {refetchData, refilterClasses, scheduleStore} from './global';
 
 // service worker
-const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            await navigator.serviceWorker.register('/sw.js', {scope: '/'});
-        } catch (error) {
-            console.error('Service worker failed to register:', error);
-        }
-    }
-};
-await registerServiceWorker();
+(async () => {
+	const registerServiceWorker = async () => {
+		if ('serviceWorker' in navigator) {
+			try {
+				await navigator.serviceWorker.register('/sw.js', {scope: '/'});
+			} catch (error) {
+				console.error('Service worker failed to register:', error);
+			}
+		}
+	};
+	await registerServiceWorker();
 
-if (!localStorage.cachedData)
-    await refetchData();
-else
-    scheduleStore.data = JSON.parse(localStorage.cachedData);
+	if (!localStorage.cachedData)
+		await refetchData();
+	else
+		scheduleStore.data = JSON.parse(localStorage.cachedData);
 
-scheduleStore.groups =
-    localStorage.userGroups ? JSON.parse(localStorage.userGroups) : [];
-scheduleStore.institution = localStorage.userInstitution ?? 13;
-scheduleStore.semester = localStorage.userSemester ?? 1;
-scheduleStore.lastUpdate = localStorage.lastUpdate;
-refilterClasses();
+	scheduleStore.groups =
+		localStorage.userGroups ? JSON.parse(localStorage.userGroups) : [];
+	scheduleStore.institution = localStorage.userInstitution ?? 13;
+	scheduleStore.semester = localStorage.userSemester ?? 1;
+	scheduleStore.lastUpdate = localStorage.lastUpdate;
+	refilterClasses();
 
-createApp(App).mount('#app')
+	createApp(App).mount('#app')
+})();
